29fe136c1fd1df45d1be4004cc604c0cb432a259
mac <mac@macs-MacBook-Pro.local>
2025-01-01 16:05:13 +0500

    new node

--- index.js
+++ index.js
@@ -27,6 +27,11 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   });
 
 // Server
+
+app.get('/', (_, res) => {
+  res.send('Server running');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
